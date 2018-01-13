78ff59e29ee7e0e2f05fc99a8d05c6872e8fe565
Yura Serdyuk <yurko7203@gmail.com>
2018-01-13 23:35:07 +0200

    first commit 320px

---
+++ js/main.js
@@ -0,0 +1,77 @@
+$('.sl').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+
+  prevArrow: false, /*вимкнута ліва стрілка*/
+  nextArrow: false, /*вимкнута права стрілка*/
+  dots: false, /*вимкнута навігація знизу*/
+});
+
+
+
+
+
+
+
+$('.sl2').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 1,
+        infinite: false,
+        focusOnSelect: true,
+        initialSlide: 1
+      }
+    }
+  ]
+});
+
+
+
+
+$('.sl3').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1,
+        infinite: false,
+        focusOnSelect: true,
+        initialSlide: 1
+      }
+    }
+  ]
+});
